5f2c497190741c847146b7c88b5670bc7d476dbc
wen <1365525513@qq.com>
2019-06-09 22:33:23 +0800

    后台增加图表

--- src/components/mylord.vue
+++ src/components/mylord.vue
@@ -44,9 +44,46 @@
       </el-header>
 
       <el-main style="padding:0">
-        <div v-show="isshow"
-             id="myChart"
-             :style="{width: '600px', height: '600px'}">
+        <div v-show="isshow">
+          <el-row :gutter="20">
+            <el-col :span="18"
+                    :offset="4">
+              <template>
+                <el-carousel :interval="4000"
+                             type="card"
+                             height="380px">
+                  <el-carousel-item v-for="item in imgarry"
+                                    :key="item">
+                    <div style="width:580px;height:380px">
+                      <img :src="item"
+                           style="width: 100%;height: 100%;"
+                           alt="">
+                    </div>
+                  </el-carousel-item>
+                </el-carousel>
+              </template>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="8">
+              <div class="grid-content bg-purple">
+                <div id="myChart"
+                     :style="{width: '400px', height: '400px'}"></div>
+              </div>
+            </el-col>
+            <el-col :span="8">
+              <div class="grid-content bg-purple">
+                <div id="myChart1"
+                     :style="{width: '400px', height: '400px'}"></div>
+              </div>
+            </el-col>
+            <el-col :span="8">
+              <div class="grid-content bg-purple">
+                <div id="myChart2"
+                     :style="{width: '500px', height: '400px'}"></div>
+              </div>
+            </el-col>
+          </el-row>
         </div>
         <el-table v-show="isshowuser"
                   :data="userdata"
@@ -355,6 +392,8 @@ export default {
   name: 'mylord',
   mounted () {
     this.drawLine()
+    this.drawLine1()
+    this.drawLine2()
     this.getalluser()
     this.getalldoctor()
     this.getalhospital()
@@ -623,24 +662,189 @@ export default {
     handleDelete (index, row) {
       console.log(index, row)
     },
-    drawLine () {
+    drawLine2 () {
       // 基于准备好的dom，初始化echarts实例
-      let myChart = this.$echarts.init(document.getElementById('myChart'))
+      let myChart = this.$echarts.init(document.getElementById('myChart2'))
 
-      // 绘制图表
-      myChart.setOption({
+      var hours = ['12a', '1a', '2a', '3a', '4a', '5a', '6a',
+        '7a', '8a', '9a', '10a', '11a',
+        '12p', '1p', '2p', '3p', '4p', '5p',
+        '6p', '7p', '8p', '9p', '10p', '11p']
+      var days = ['Saturday', 'Friday', 'Thursday',
+        'Wednesday', 'Tuesday', 'Monday', 'Sunday']
+
+      var data = [[0, 0, 5], [0, 1, 1], [0, 2, 0], [0, 3, 0], [0, 4, 0], [0, 5, 0], [0, 6, 0], [0, 7, 0], [0, 8, 0], [0, 9, 0], [0, 10, 0], [0, 11, 2], [0, 12, 4], [0, 13, 1], [0, 14, 1], [0, 15, 3], [0, 16, 4], [0, 17, 6], [0, 18, 4], [0, 19, 4], [0, 20, 3], [0, 21, 3], [0, 22, 2], [0, 23, 5], [1, 0, 7], [1, 1, 0], [1, 2, 0], [1, 3, 0], [1, 4, 0], [1, 5, 0], [1, 6, 0], [1, 7, 0], [1, 8, 0], [1, 9, 0], [1, 10, 5], [1, 11, 2], [1, 12, 2], [1, 13, 6], [1, 14, 9], [1, 15, 11], [1, 16, 6], [1, 17, 7], [1, 18, 8], [1, 19, 12], [1, 20, 5], [1, 21, 5], [1, 22, 7], [1, 23, 2], [2, 0, 1], [2, 1, 1], [2, 2, 0], [2, 3, 0], [2, 4, 0], [2, 5, 0], [2, 6, 0], [2, 7, 0], [2, 8, 0], [2, 9, 0], [2, 10, 3], [2, 11, 2], [2, 12, 1], [2, 13, 9], [2, 14, 8], [2, 15, 10], [2, 16, 6], [2, 17, 5], [2, 18, 5], [2, 19, 5], [2, 20, 7], [2, 21, 4], [2, 22, 2], [2, 23, 4], [3, 0, 7], [3, 1, 3], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 1], [3, 9, 0], [3, 10, 5], [3, 11, 4], [3, 12, 7], [3, 13, 14], [3, 14, 13], [3, 15, 12], [3, 16, 9], [3, 17, 5], [3, 18, 5], [3, 19, 10], [3, 20, 6], [3, 21, 4], [3, 22, 4], [3, 23, 1], [4, 0, 1], [4, 1, 3], [4, 2, 0], [4, 3, 0], [4, 4, 0], [4, 5, 1], [4, 6, 0], [4, 7, 0], [4, 8, 0], [4, 9, 2], [4, 10, 4], [4, 11, 4], [4, 12, 2], [4, 13, 4], [4, 14, 4], [4, 15, 14], [4, 16, 12], [4, 17, 1], [4, 18, 8], [4, 19, 5], [4, 20, 3], [4, 21, 7], [4, 22, 3], [4, 23, 0], [5, 0, 2], [5, 1, 1], [5, 2, 0], [5, 3, 3], [5, 4, 0], [5, 5, 0], [5, 6, 0], [5, 7, 0], [5, 8, 2], [5, 9, 0], [5, 10, 4], [5, 11, 1], [5, 12, 5], [5, 13, 10], [5, 14, 5], [5, 15, 7], [5, 16, 11], [5, 17, 6], [5, 18, 0], [5, 19, 5], [5, 20, 3], [5, 21, 4], [5, 22, 2], [5, 23, 0], [6, 0, 1], [6, 1, 0], [6, 2, 0], [6, 3, 0], [6, 4, 0], [6, 5, 0], [6, 6, 0], [6, 7, 0], [6, 8, 0], [6, 9, 0], [6, 10, 1], [6, 11, 0], [6, 12, 2], [6, 13, 1], [6, 14, 3], [6, 15, 4], [6, 16, 0], [6, 17, 0], [6, 18, 0], [6, 19, 0], [6, 20, 1], [6, 21, 2], [6, 22, 2], [6, 23, 6]]
+
+      data = data.map(function (item) {
+        return [item[1], item[0], item[2] || '-']
+      })
+
+      var option = {
+        title: {
+          text: '医院患者热力图',
+          left: 'center',
+          top: 20,
+          textStyle: {
+            color: 'black'
+          }
+        },
+        tooltip: {
+          position: 'top'
+        },
+        animation: false,
+        grid: {
+          height: '50%',
+          y: '10%'
+        },
         xAxis: {
           type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+          data: hours,
+          splitArea: {
+            show: true
+          }
+        },
+        yAxis: {
+          type: 'category',
+          data: days,
+          splitArea: {
+            show: true
+          }
+        },
+        visualMap: {
+          min: 0,
+          max: 10,
+          calculable: true,
+          orient: 'horizontal',
+          left: 'center',
+          bottom: '15%'
+        },
+        series: [{
+          name: 'Punch Card',
+          type: 'heatmap',
+          data: data,
+          label: {
+            normal: {
+              show: true
+            }
+          },
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }]
+      }
+      // 绘制图表
+      myChart.setOption(option)
+    },
+    drawLine1 () {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById('myChart1'))
+      var option = {
+        title: {
+          text: '医院患者流量表',
+          left: 'center',
+          top: 20,
+          textStyle: {
+            color: 'black'
+          }
+        },
+        xAxis: {
+          type: 'category',
+          data: ['6-4', '6-5', '6-6', '6-7', '6-8', '6-9', '6-10']
         },
         yAxis: {
           type: 'value'
         },
         series: [{
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          data: [20, 32, 1, 34, 90, 30, 20],
           type: 'line'
         }]
-      })
+      }
+
+      // 绘制图表
+      myChart.setOption(option)
+    },
+    drawLine () {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById('myChart'))
+      var option = {
+        backgroundColor: '#2c343c',
+
+        title: {
+          text: '医生分布',
+          left: 'center',
+          top: 20,
+          textStyle: {
+            color: '#ccc'
+          }
+        },
+
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+
+        visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+            colorLightness: [0, 1]
+          }
+        },
+        series: [
+          {
+            name: '医生分布',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '50%'],
+            data: [
+              { value: 3, name: '眼科' },
+              { value: 2, name: '脑科' },
+              { value: 2, name: '呼吸科' },
+              { value: 2, name: '心脏科' },
+              { value: 4, name: '牙科' },
+              { value: 5, name: '遗传科' },
+              { value: 1, name: '肝胆科' }
+            ].sort(function (a, b) { return a.value - b.value }),
+            roseType: 'radius',
+            label: {
+              normal: {
+                textStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                lineStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: '#c23533',
+                shadowBlur: 200,
+                shadowColor: 'rgba(0, 0, 0, 0.3)'
+              }
+            },
+
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function (idx) {
+              return Math.random() * 200
+            }
+          }
+        ]
+      }
+      // 绘制图表
+      myChart.setOption(option)
     },
     getalluser () {
       this.$api.get(
@@ -735,7 +939,13 @@ export default {
       dialogFormdoctor: false,
       dialogFormhospital: false,
       tceditobj: { id: null, username: null, sex: null, rfid: null, adress: null, phone: null, password: null, age: null, isadmin: null },
-      tceditobjdoctor: { ysid: null, name: null, age: null, img: null, sex: null, office: null, abstract: null, hsid: null }
+      tceditobjdoctor: { ysid: null, name: null, age: null, img: null, sex: null, office: null, abstract: null, hsid: null },
+      imgarry: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560646720&di=ba2169fba50b0ff87a23827975a98c95&imgtype=jpg&er=1&src=http%3A%2F%2Fa1.att.hudong.com%2F40%2F12%2F01300541222586139240128480777.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560052002092&di=ef2074ea9f5f39176fff12887c3f53f9&imgtype=0&src=http%3A%2F%2Fiinfo.zhulong.com%2Fstatic%2Ftech%2Fnew_miniature%2F2510%2F201522174653584_14.jpg%3Ff%3D3',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560052002089&di=6d9c00a5a431473ee99a4261476409e3&imgtype=0&src=http%3A%2F%2Fstatic.jstv.com%2Fimg%2F2018%2F11%2F18%2F201811181542511961287_26.jpeg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560052002076&di=b2563f9dd0da8ea3e41c0d6c127bba3b&imgtype=0&src=http%3A%2F%2Ffile1.sheencity.com%2Fdata%2Fbig%2F2012-11%2F01%2F1f11cb25162e874b5c36870ae6ca914a.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560052448820&di=1ca6e5cfa8455925f7219b450a45070d&imgtype=0&src=http%3A%2F%2Fwww.towntour.net%2Fd%2Ffile%2Frmtj%2F2019-06-07%2F109d569f016adc35ca641e3f634f216d.jpg'],
+      mytime: new Date()
 
     }
   },
